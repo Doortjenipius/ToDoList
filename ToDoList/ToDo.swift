1d1e26e910ce54562d8ca01d8fcdce7111f266a4
doortje <doortje@wcw-stud-145-109-27-148.wireless.uva.nl>
2018-11-28 12:41:39 +0100

    Jaaa

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,59 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by doortje on 27/11/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo: Codable {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "ToDo 1", isComplete: false,
+                         dueDate: Date(), notes: "Leuk bericht")
+        let todo2 = ToDo(title: "ToDo 2", isComplete: false,
+                         dueDate: Date(), notes: "Komt goed")
+        let todo3 = ToDo(title: "ToDo 3", isComplete: false,
+                         dueDate: Date(), notes: "Oh shit")
+  
+        return [todo1, todo2, todo3]
+    }
+    
+    static func loadToDos() -> [ToDo]?  {
+        guard let codedToDos = try? Data(contentsOf: ArchiveURL)
+            else {return nil}
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode(Array<ToDo>.self,
+                                               from: codedToDos)
+    }
+
+    static let dueDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    static let DocumentsDirectory =
+        FileManager.default.urls(for: .documentDirectory,
+                                 in: .userDomainMask).first!
+    static let ArchiveURL =
+        DocumentsDirectory.appendingPathComponent("todos")
+            .appendingPathExtension("plist")
+    
+    static func saveToDos(_ todos: [ToDo]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let codedToDos = try? propertyListEncoder.encode(todos)
+        try? codedToDos?.write(to: ArchiveURL,
+                               options: .noFileProtection)
+    }
+    
+
+}
---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by doortje on 28/11/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import UIKit
+
+
+@objc protocol ToDoCellDelegate: class {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func completeButtonTapped(_ sender: Any) {
+    
+        delegate?.checkmarkTapped(sender: self)
+    }
+}
